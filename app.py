6ababb0c42231ff7de39884e574a385e34f889dc
Gabriel Nuernberg Biazoto <biazotogabriel@gmail.com>
2023-03-03 14:35:39 -0300

    adicionado seção de respostas

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def create_dataframe_section(df):
 
 def create_answers_section(df):
     st.title("Main Questions Answers")
-
     st.header("First Round")
+
     st.subheader("How many bikes are being sold by their owners and how many bikes are being sold by distributors?")
 
     st.subheader("How many bikes are being sold are bikes from a unique owner?")
@@ -61,8 +61,8 @@ def create_answers_section(df):
 
 def main():
     df = load_data()
-
     create_dataframe_section(df)
+    create_answers_section(df)
 
     st.dataframe(df)    
     
